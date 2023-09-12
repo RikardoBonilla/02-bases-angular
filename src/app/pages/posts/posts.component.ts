c82f2cfb2b491cea90971b37e7cee9857292072f
Ricardo <ricardoandresbonilla@gmai.com>
2023-09-11 20:59:02 -0500

    first commit

---
+++ src/app/pages/posts/posts.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+
+export class PostsComponent implements OnInit {
+
+  mensajes: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.mensajes = this.dataService.getPosts();
+  }
+
+  escucharClick( id: number ){
+    console.log('Click en ', id)
+  }
+}
